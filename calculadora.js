2d52867926004ecc43234dc23a9201263d48d23e
OlgaPio <lm4109080@gmail.com>
2025-05-19 01:06:48 -0500

    paso 2: funcion para realizar operaciones

--- calculadora.js
+++ calculadora.js
@@ -2,3 +2,22 @@
 let num1 = 10; 
 let num2 = 5;
 let operacion = "suma"; 
+
+// Paso 2: Función para realizar operaciones
+function realizarOperacion(num1, num2, operacion) {
+    if (operacion === "suma") {
+        return num1 + num2;
+    } else if (operacion === "resta") {
+        return num1 - num2;
+    } else if (operacion === "multiplicacion") {
+        return num1 * num2;
+    } else if (operacion === "division") {
+        if (num2 !== 0) {
+            return num1 / num2;
+        } else {
+            return "Error: No se puede dividir por cero.";
+        }
+    } else {
+        return "Error: Operación no válida.";
+    }
+}
